e39140237751909ae4279da3de999e1a67712b72
Natalia <nataliamisiewicz1@gmail.com>
2018-01-14 15:02:41 +0100

    Tooltips update

--- Warsztaty/5_Tooltips/js/app.js
+++ Warsztaty/5_Tooltips/js/app.js
@@ -6,3 +6,23 @@
 /*
  <span class="tooltipText">Text tooltipa</span>
  */
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Działa!");
+
+    var tooltips = document.querySelectorAll('.tooltip');
+    //console.log(tooltips);
+    var newSpan = document.createElement('span');
+
+    tooltips.forEach(function (el) {
+        el.addEventListener('mouseover', function () {
+            newSpan.innerText = this.dataset.text;
+            newSpan.classList.add('tooltipText');
+            el.appendChild(newSpan);
+        });
+
+        el.addEventListener('mouseout', function () {
+            newSpan.remove();
+        });
+    });
+
+});
